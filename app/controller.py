431857cdf6f8b25bf151da56a142b555bf466e84
Zeta90 <davidoff.igle@gmail.com>
2020-06-21 08:51:53 +0100

    WaveSaper

--- app/controller.py
+++ app/controller.py
@@ -6,124 +6,169 @@ from app.simulator.simmaths import Simmaths
 import numpy as np
 import json
 import math
-from django.views.decorators.csrf import csrf_exempt 
+from django.views.decorators.csrf import csrf_exempt
 
 SAMPLES = 200
 
+
 class SimulatorController:
-    def run(self, delta_t,system_acc,system_braking,system_damping,system_long):
-        self.system_delta_t = delta_t
-        self.system_acc =system_acc
-        self.system_braking = system_braking
-        self.system_damping = system_damping
-        self.system_long = system_long
-        
-        self.system_samplingF = delta_t*SAMPLES
-        
+    def run(self,delta_t):
+        self.SAMPLES = 200
+        self.SYSTEM_DELTA_T = delta_t
+        self.system_acc = 0
+        self.system_braking = 0
+        self.system_damping = 0
+        self.system_long = 0
+
+        #self.system_samplingF = delta_t * SAMPLES
+
         self.sim_time = []
-        self.sim_x = []
+        self.sim_y = []
         self.sim_velocity = []
-        
+        self.sim_acceleration = []
+
         self.sim_static_output = []
         self.sim_static_spectrum = []
         self.sim_static_freqs = []
 
         self.sim_static_optimal_spectrum = []
-        
+
         self.fourier = None
-        
+
+
 sController = SimulatorController()
 
+
 def calculate_optimus_static():
-    #print(222)
-    system_static_spectrum = [[],[]]
-    #print(1125)
-    freq = np.arange(0,(SAMPLES/2)) * 0.1
-    fourier= Fourier()
+    # print(222)
+    system_static_spectrum = [[], []]
+    # print(1125)
+    freq = np.arange(0, (SAMPLES / 2)) * 0.1
+    fourier = Fourier()
     for i in range(int(SAMPLES)):
-        if i==0 or i ==1:
+        if i == 0 or i == 1:
             system_static_spectrum[0].append([0])
             system_static_spectrum[1].append(freq.tolist())
-            #print(system_static_spectrum)
+            # print(system_static_spectrum)
         else:
-            #print('->>')
-            #print(system_static_spectrum)
+            # print('->>')
+            # print(system_static_spectrum)
             system_y_step = sController.sim_static_output[:i]
-            #print(system_y_step)           
+            # print(system_y_step)
 
-            sim_static_optimal_spectrum=fourier.frequecies_spectrum(system_y_step,sController.system_samplingF)
-            #print(sController.sim_static_optimal_spectrum)
-            fft_amp = (abs(sim_static_optimal_spectrum[0]))
-            
-            #fft_freq = (sController.sim_static_optimal_spectrum[1]).tolist()
+            sim_static_optimal_spectrum = fourier.frequecies_spectrum(
+                system_y_step, sController.system_samplingF
+            )
+            # print(sController.sim_static_optimal_spectrum)
+            fft_amp = abs(sim_static_optimal_spectrum[0])
+
+            # fft_freq = (sController.sim_static_optimal_spectrum[1]).tolist()
             system_static_spectrum[0].append(list(fft_amp))
-            #print(list(fft_amp))
-            #system_static_spectrum[0][i]=(fft_amp)
-            #print(fft_amp)
-            #system_static_spectrum[1][i]=(fft_freq)
-            #sController.sim_static_spectrum=sController.sim_static_spectrum.astype('float64') 
-            #print(fft_amp)
-            #print(fft_freq)
-    #print(system_static_spectrum[0][:2])
-    #print(sController.sim_static_optimal_spectrum)
-    triangle = Simmaths.calc_response_angle_sbs(sController.sim_x,sController.sim_static_output,sController.sim_x,sController.system_long)
-    sgnl = [json.dumps([sController.sim_static_output,sController.sim_time,system_static_spectrum[0],sController.sim_static_freqs,triangle])]
+            # print(list(fft_amp))
+            # system_static_spectrum[0][i]=(fft_amp)
+            # print(fft_amp)
+            # system_static_spectrum[1][i]=(fft_freq)
+            # sController.sim_static_spectrum=sController.sim_static_spectrum.astype('float64')
+            # print(fft_amp)
+            # print(fft_freq)
+    # print(system_static_spectrum[0][:2])
+    # print(sController.sim_static_optimal_spectrum)
+    triangle = Simmaths.calc_response_angle_sbs(
+        sController.sim_y,
+        sController.sim_static_output,
+        sController.sim_y,
+        sController.system_long,
+    )
+    sgnl = [
+        json.dumps(
+            [
+                sController.sim_static_output,
+                sController.sim_time,
+                system_static_spectrum[0],
+                sController.sim_static_freqs,
+                triangle,
+            ]
+        )
+    ]
     return sgnl
-        
+
+
 @csrf_exempt
 def AJAX_generate_signal(request):
-    system_delta_t = float(request.POST.get('delta_t'))
-    system_damping = float(request.POST.get('system_damping'))
-    system_acc = float(request.POST.get('system_acc'))
-    system_long = float(request.POST.get('system_long'))
-    system_braking = float(request.POST.get('system_braking'))
-    
-    sController.run(system_delta_t,system_acc,system_braking,system_damping,system_long)
+    signal_type = request.POST.get("signal_type")
+    signal_data = request.POST.get("signal_data")
+    signal_data = json.loads(signal_data)
+    # system_delta_t = float(request.POST.get("delta_t"))
+    # system_damping = float(request.POST.get("system_damping"))
+    # system_acc = float(request.POST.get("system_acc"))
+    # system_long = float(request.POST.get("system_long"))
+    # system_braking = float(request.POST.get("system_braking"))
 
-    signal = SignalGenerator(system_delta_t,system_acc,system_braking)
-    system_x,system_t,system_v = signal.generate_signal_values(SAMPLES)
-    
+    sController.run(0.1)
+
+    signal = SignalGenerator(sController.SYSTEM_DELTA_T, sController.SAMPLES)
+    system_t, system_y, system_v,system_a = signal.generate_signal_values(signal_type, signal_data)
+
     sController.sim_time = system_t
-    sController.sim_x = system_x
+    sController.sim_y = system_y
     sController.sim_velocity = system_v
-
-    sgnl = [json.dumps([system_x,system_t,system_v])]
-    #print(sgnl)
+    sController.sim_acceleration = system_a
+    sgnl = [json.dumps([system_t, system_y, system_v,system_a])]
+    # print(sgnl)
+    signal = None
     return HttpResponse(sgnl)
 
-@csrf_exempt
-def AJAX_static_optimal_input(request):
-    #print(sController.sim_input)
-    optimal_static_spectrum = calculate_optimus_static()
-    #print(optimal_static_spectrum)
-    return HttpResponse(optimal_static_spectrum)
 
-@csrf_exempt
-def AJAX_static_process_input(request):
-    #print(sController.sim_time)
-    #print(len(sController.sim_time))
-    LinearSimulation = Pylsa(sController.system_delta_t)
-    LinearSimulation.transferFunction_standard(math.sqrt(9.81 / sController.system_long), sController.system_damping, 1)
-    response = LinearSimulation.linear_simulation(sController.sim_x, sController.sim_time, sController.system_delta_t, SAMPLES)
-    sController.sim_static_output = response
-    #print(response)
-    
-    sController.fourier = Fourier()
-    sController.sim_static_spectrum=sController.fourier.frequecies_spectrum(response,sController.system_samplingF)
-    #print(sController.sim_static_spectrum)
-    fft_amp = (abs(sController.sim_static_spectrum[0]).tolist())
-    fft_freq = (sController.sim_static_spectrum[1]).tolist()
-    #sController.sim_static_spectrum=sController.sim_static_spectrum.astype('float64') 
-    #print(fft_amp)
-    #print(fft_freq)
+# @csrf_exempt
+# def AJAX_static_optimal_input(request):
+#     # print(sController.sim_input)
+#     optimal_static_spectrum = calculate_optimus_static()
+#     # print(optimal_static_spectrum)
+#     return HttpResponse(optimal_static_spectrum)
 
-    sController.sim_static_freqs = fft_freq
-    sgnl = [json.dumps([response,sController.sim_time,fft_amp,fft_freq])]
-    
-    
-    #print(sgnl)
-    return HttpResponse(sgnl)
-    
+
+# @csrf_exempt
+# def AJAX_static_process_input(request):
+#     # print(sController.sim_time)
+#     # print(len(sController.sim_time))
+#     LinearSimulation = Pylsa(sController.system_delta_t)
+#     w2 = 9.81 / sController.system_long
+#     LinearSimulation.transferFunction_standard(
+#         math.sqrt(w2), sController.system_damping, 1
+#     )
+#     response = LinearSimulation.linear_simulation(
+#         sController.sim_y, sController.sim_time, sController.system_delta_t, SAMPLES
+#     )
+#     sController.sim_static_output = response
+#     # print(response)
+
+#     sController.fourier = Fourier()
+#     sController.sim_static_spectrum = sController.fourier.frequecies_spectrum(
+#         response, sController.system_samplingF
+#     )
+#     # print(sController.sim_static_spectrum)
+#     fft_amp = abs(sController.sim_static_spectrum[0]).tolist()
+#     fft_freq = (sController.sim_static_spectrum[1]).tolist()
+#     # sController.sim_static_spectrum=sController.sim_static_spectrum.astype('float64')
+#     # print(fft_amp)
+#     # print(fft_freq)
+
+#     sController.sim_static_freqs = fft_freq
+#     sgnl = [json.dumps([response, sController.sim_time, fft_amp, fft_freq])]
+
+#     # print(sgnl)
+#     return HttpResponse(sgnl)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -148,11 +193,11 @@ def AJAX_static_process_input(request):
 #     response = LinearSimulation.linear_simulation(system_y, system_t, system_delta_t, 2000)
 
 #     fourier = Fourier()
-   
+
 #     static_spectrum=fourier.frequecies_spectrum(system_y)
-    
+
 #     optimal_static_spectrum = calculate_optimus_static(system_y,fourier)
-    
+
 #     # data_np = np.array(static_spectrum)
 #     # data_list = data_np.tolist()
 #     # static_spectrum == data_list
@@ -163,4 +208,3 @@ def AJAX_static_process_input(request):
 #                         , optimal_static_spectrum[0].tolist(),optimal_static_spectrum[1].tolist()
 #                         ])
 #     return HttpResponse(input)
-    
--- app/simulator/signal_generator.py
+++ app/simulator/signal_generator.py
@@ -1,36 +1,129 @@
 from app.simulator.physics import Physics
 
+
 class SignalGenerator:
     x = []
     v = []
-    t = []
+    y = []
+    a = []
+
+    TIME_MULTIPLIER = 0
+    SAMPLES = 0
+    TOTAL_SIMULATION_TIME = 0
+    EFFECTIVE_SIMULATION_TIME = 0
 
     delta_t = 0
     acceleration = 0
     braking = 0
 
-    def __init__(self,delta_t,acceleration, braking):
+    def __init__(self, delta_t, samples):
         self.delta_t = delta_t
-        self.acceleration = acceleration
-        self.braking = braking
-        self.physics = Physics(delta_t,acceleration,braking)
+        self.SAMPLES = samples
+        self.TOTAL_SIMULATION_TIME = self.SAMPLES * self.delta_t
+        self.TIME_MULTIPLIER = samples / self.TOTAL_SIMULATION_TIME
 
-    def generate_signal_values(self,smpls):
-        self.x = [0]
+        self.physics = Physics(delta_t, self.TIME_MULTIPLIER)
+
+    def generate_signal_values(self, signal_type, signal_data):
+        self.y = [0]
         self.v = [0]
-        self.t = []
-        for i in range(smpls):
-            if(i<(smpls/10)):
-                x, v = self.physics.Right(i,self.delta_t)
-                t = i * self.delta_t
+        self.a = [0]
+        self.x = []
+        if (signal_type) == "1":
+            offset_samples = float(signal_data[0]) / self.delta_t
+            offset_time = float(signal_data[0])
+            amp = float(signal_data[1])
+            self.x, self.y, self.v, self.a = self.step(offset_time, offset_samples, amp)
+        elif (signal_type) == "2":
+            offset_samples = float(signal_data[0]) / self.delta_t
+            offset_time = float(signal_data[0])
+            s_on_time = float(signal_data[1])
+            s_off_time = float(signal_data[2])
+            amp = float(signal_data[3])
+            self.x, self.y, self.v, self.a = self.square(
+                offset_time, offset_samples, s_on_time, s_off_time, amp
+            )
+            # for i in range(samples):
+            #     if i < (t0):
+            #         x, v = self.physics.Right(i, self.delta_t)
+            #         x *= gain
+            #         v *= gain
+            #         t = i * self.delta_t
+            #         self.x.append(x)
+            #         self.v.append(v)
+            #         self.t.append(t)
+            #     elif i < (samples - 1):
+            #         x, v = self.physics.Middle(i, self.delta_t)
+            #         x *= gain
+            #         v *= gain
+            #         self.x.append(x)
+            #         self.v.append(v)
+            #         self.t.append(t)
+            # print((self.t))
+        return self.x, self.y, self.v, self.a
+
+    def square(self, offset_time, offset_samples, s_on_time, s_off_time, amp):
+        i = 0
+        up = false
+        for i in range(self.SAMPLES):
+            if i < (offset_samples):
+                x = i * self.delta_t
+                y = 0
+                v = 0
+                a = 0
                 self.x.append(x)
+                self.y.append(y)
                 self.v.append(v)
-                self.t.append(t)
-            elif(i<(smpls-1)):
-                x, v = self.physics.Middle(i,self.delta_t)
-                t = i * self.delta_t
+                self.a.append(a)
+            elif i < (self.SAMPLES - 1):
+                j = i-offset_samples
+                if (j % s_on_time) == 0:
+                    up = True
+                elif (j % s_off_time) == 0:
+                    up = False
+                
+            if up == True:
+                x = (i) * self.delta_t
+                y = amp
+                v = self.physics.step_v(x, self.TIME_MULTIPLIER)
+                a = self.physics.step_a(v, self.TIME_MULTIPLIER)
+            else:
+                x = (i) * self.delta_t
+                y = 0
+                v = 0
+                a = self.physics.step_a(v, self.TIME_MULTIPLIER)
+
                 self.x.append(x)
+                self.y.append(y)
                 self.v.append(v)
-                self.t.append(t)
-        print((self.x))
-        return self.x, self.t, self.v
+                self.a.append(a)
+        self.x.append(self.TOTAL_SIMULATION_TIME)
+        # print((self.t))
+        return self.x, self.y, self.v, self.a
+
+    def step(self, offset_time, offset_samples, amp):
+        print("****")
+        print(offset_samples)
+        i = 0
+        for i in range(self.SAMPLES):
+            if i < (offset_samples):
+                x = i * self.delta_t
+                y = 0
+                v = 0
+                a = 0
+                self.x.append(x)
+                self.y.append(y)
+                self.v.append(v)
+                self.a.append(a)
+            elif i < (self.SAMPLES - 1):
+                x = (i) * self.delta_t
+                y = amp
+                v = self.physics.step_v(x, self.TIME_MULTIPLIER)
+                a = self.physics.step_a(v, self.TIME_MULTIPLIER)
+                self.x.append(x)
+                self.y.append(y)
+                self.v.append(v)
+                self.a.append(a)
+        self.x.append(self.TOTAL_SIMULATION_TIME)
+        # print((self.t))
+        return self.x, self.y, self.v, self.a
